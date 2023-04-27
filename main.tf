64413964bfac6c419cbf369c632f8fb80435b14c
Gil Megidish <gil@megidish.net>
2023-04-27 20:58:49 +0300

    Trying aws provider

--- main.tf
+++ main.tf
@@ -27,3 +27,42 @@ provider "aws" {
 resource "aws_s3_bucket" "dummy-bucket" {
   bucket = "delete-me-bucket-2023-04-27"
 }
+
+
+
+resource "aws_cloudfront_distribution" "dummy_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.dummy-bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = "my_s3_origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "my_s3_origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+
