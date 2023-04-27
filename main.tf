c8702f0886966e95bef0d9ea47cf92af3e91eef2
Gil Megidish <gil@megidish.net>
2023-04-27 20:50:41 +0300

    Trying aws provider

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "better-goals-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 # Configure the AWS Provider
