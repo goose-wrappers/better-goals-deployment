dffc857eee32dc225ec92bba5947609fae16d3ad
Gil Megidish <gil@megidish.net>
2023-04-27 22:15:49 +0300

    Minor refactor

--- main.tf
+++ main.tf
@@ -1,33 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "better-goals-terraform"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-
-
-
-
-
 resource "aws_s3_bucket" "dummy-bucket" {
   bucket = "delete-me-bucket-2023-04-27"
 }
-
 
 resource "aws_cloudfront_origin_access_control" "cloudfront_acl" {
   name                              = "example"
@@ -103,7 +76,6 @@ data "aws_iam_policy_document" "iam_s3_bucket_policy" {
     }
   }
 }
-
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.dummy-bucket.id
---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "better-goals-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
