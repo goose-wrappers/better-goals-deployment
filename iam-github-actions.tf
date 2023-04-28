2d7347464ce941b62afe0938c664308e76cc4709
Gil Megidish <gil@megidish.net>
2023-04-28 16:41:44 +0300

    Create an IAM user for github

---
+++ iam-github-actions.tf
@@ -0,0 +1,19 @@
+# this terraform will create a user that will be used from github actions to upload files to S3
+
+resource "aws_iam_user" "github_actions_iam" {
+  name = "github-actions-bot"
+}
+
+data "aws_iam_policy_document" "github_actions_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject", "s3:PutObjectAcl", "s3:ListAllMyBuckets"]
+    resources = [aws_s3_bucket.dummy-bucket.arn]
+  }
+}
+
+resource "aws_iam_user_policy" "github_actions_policy" {
+  name   = "test"
+  user   = aws_iam_user.github_actions_iam.name
+  policy = data.aws_iam_policy_document.github_actions_policy_document.json
+}
--- main.tf
+++ main.tf
@@ -2,58 +2,6 @@ resource "aws_s3_bucket" "dummy-bucket" {
   bucket = "delete-me-bucket-2023-04-27"
 }
 
-resource "aws_cloudfront_origin_access_control" "cloudfront_acl" {
-  name                              = "example"
-  description                       = "Example Policy"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-resource "aws_cloudfront_distribution" "dummy_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket.dummy-bucket.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_acl.id
-    origin_id                = "my_s3_origin"
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    compress         = true
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "my_s3_origin"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
-
-
 data "aws_iam_policy_document" "iam_s3_bucket_policy" {
   statement {
     principals {
