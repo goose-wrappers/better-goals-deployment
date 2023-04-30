875d8cfa8f300b566850ec8629a6efe99c004add
Gil Megidish <gil@megidish.net>
2023-04-30 16:57:08 +0300

    Trying to fix lambda

--- iam-github-actions.tf
+++ iam-github-actions.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "github_actions_policy_document" {
   statement {
     effect    = "Allow"
     actions   = ["lambda:CreateFunction", "lambda:UpdateFunction"]
-    resources = ["arn:aws:lambda:::function:better-goals"]
+    resources = ["*"]
   }
 
   statement {
