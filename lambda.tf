da698c491a729f703609cceb9889886370c895e7
Gil Megidish <gil@megidish.net>
2023-04-30 15:34:16 +0300

    Removed lambda stuff, will be done through github action shell instead

--- lambda.tf
+++
@@ -1,7 +0,0 @@
-#resource "aws_lambda_function" "lambda_function" {
-#  function_name = "better-goals"
-#  role          = aws_iam_role.iam_for_lambda.arn
-#  handler       = "index.handler"
-#  filename      = "function.zip"
-#  runtime       = "nodejs16.x"
-#}
