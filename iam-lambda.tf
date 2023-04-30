635832362dd1d2b5349dfba2f74f117c40b67f87
Gil Megidish <gil@megidish.net>
2023-04-30 16:14:49 +0300

    Trying to fix lambda

--- iam-lambda.tf
+++ iam-lambda.tf
@@ -26,13 +26,19 @@ data "aws_iam_policy_document" "iam_invoke_function_policy" {
   }
 }
 
+resource "aws_iam_policy" "policy_for_lambda" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy      = data.aws_iam_policy_document.iam_invoke_function_policy
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.iam_for_lambda_policy.json
 }
 
-resource "aws_iam_policy_attachment" "invoke_policy_for_lambda" {
+resource "aws_iam_policy_attachment" "policy_attachment_for_lamda" {
   name       = "invoke_policy_for_lambda"
   roles      = [aws_iam_role.iam_for_lambda.name]
-  policy_arn = data.aws_iam_policy_document.iam_invoke_function_policy.json
+  policy_arn = aws_iam_policy.policy_for_lambda.arn
 }
