f8869b19e121311d623ec1eef5143f2f5dae4fe7
Gil Megidish <gil@megidish.net>
2023-04-30 16:03:02 +0300

    Trying to fix lambda

--- iam-lambda.tf
+++ iam-lambda.tf
@@ -34,4 +34,5 @@ resource "aws_iam_role" "iam_for_lambda" {
 resource "aws_iam_policy_attachment" "invoke_policy_for_lambda" {
   name       = "invoke_policy_for_lambda"
   policy_arn = data.aws_iam_policy_document.iam_invoke_function_policy.json
+  roles      = [aws_iam_role.iam_for_lambda]
 }
