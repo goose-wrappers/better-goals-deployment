e04dbd201d98faffca25957b96bad3c78259cc90
Gil Megidish <gil@megidish.net>
2023-05-22 22:28:05 +0300

    Added S3 bucket for canary results

--- s3.tf
+++ s3.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "better_goals_s3" {
   bucket = "better-goals-cdn"
 }
+
+resource "aws_s3_bucket" "better_goals_canary_results_s3" {
+  bucket = "better-goals-canary-results"
+}
