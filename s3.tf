86e1b3c9a76d55a37bd19d7d454557a6397d5f00
Gil Megidish <gil@megidish.net>
2023-05-22 22:50:48 +0300

    Renamed bucket

--- s3.tf
+++ s3.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "better_goals_s3" {
   bucket = "better-goals-cdn"
 }
 
-resource "aws_s3_bucket" "better_goals_canary_results_s3" {
-  bucket = "better-goals-canary-results"
+resource "aws_s3_bucket" "better_goals_canary_output_s3" {
+  bucket = "better-goals-canary-output"
 }
