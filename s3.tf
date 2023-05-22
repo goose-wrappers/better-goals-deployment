d43557f1f30a1f00c83f807f2dca2d6cd6622f03
Gil Megidish <gil@megidish.net>
2023-05-22 22:30:01 +0300

    Force region to us-east-1

--- s3.tf
+++ s3.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "better_goals_s3" {
   bucket = "better-goals-cdn"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "better_goals_canary_results_s3" {
   bucket = "better-goals-canary-results"
+  region = "us-east-1"
 }
